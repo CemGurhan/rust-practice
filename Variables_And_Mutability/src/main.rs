518068963da692cfc169ab983de18f3c9d350931
CemGurhan <cemg@live.co.uk>
2023-02-26 11:54:18 +0000

    added global constant example

--- Variables_And_Mutability/src/main.rs
+++ Variables_And_Mutability/src/main.rs
@@ -1,3 +1,5 @@
+const GLOBAL_CONSTANT_EXAMPLE : &str = "I am a global constant";
+
 fn main() {
     // VARIABLES:
     // variables are immutable by default, but can be made mutable with the `mut` keyword
@@ -13,7 +15,5 @@ fn main() {
     // Rusts naming convention states that constants must be all upper case with underscores between words
     const THREE_HOURS_IN_SECONDS : u32 = 3 * 60 * 60; 
     println!("Three hours in seconds: {}", THREE_HOURS_IN_SECONDS);
-
-
-
+    println!("From Global: {}", GLOBAL_CONSTANT_EXAMPLE);
 }
