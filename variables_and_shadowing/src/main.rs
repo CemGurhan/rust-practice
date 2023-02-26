cedd1405f5f536cf403412412220e20aba67fa9d
CemGurhan <cemg@live.co.uk>
2023-02-26 13:26:34 +0000

    added comments on shadowed variable scopes

--- variables_and_shadowing/src/main.rs
+++ variables_and_shadowing/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
+    // Shadowing allows us to create a new instance of a variable with the same name as before.
+    // It allows us to assign new functionality to a variable
     let x = 22;
     let x = 23;
+    // The value of a shadowed variable is scoped, such that once this inner scope ends, 
+    // the variable shadowing the previous on from the outer scope is dropped and
+    // the value returns back to what it was before the inner scope
     {
         let x = 23 * 2; 
         println!("The value of x in inner scope is: {}", x);
