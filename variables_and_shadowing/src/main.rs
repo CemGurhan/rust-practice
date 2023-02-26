66dd8446d667509c0a286ced119a39dff87408b8
CemGurhan <cemg@live.co.uk>
2023-02-26 13:30:26 +0000

    added shadowed variable type change example and to original shadowed variable example's comments

--- variables_and_shadowing/src/main.rs
+++ variables_and_shadowing/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     // Shadowing allows us to create a new instance of a variable with the same name as before.
-    // It allows us to assign new functionality to a variable
+    // It allows us to assign new functionality to a variable whilst also retaining it's immutability
     let x = 22;
     let x = 23;
     // The value of a shadowed variable is scoped, such that once this inner scope ends, 
@@ -11,4 +11,11 @@ fn main() {
         println!("The value of x in inner scope is: {}", x);
     }
     println!("The value of x in outer scope is: {}", x);
+
+    // We can use variable shadowing to change the type of a variable with the same name.
+    // This is useful, as we dont have to change the name of a variable just to change it's type
+    let space_value = "    ";
+    let space_value = space_value.len();
+
+    println!("Amount of space in original shadowed variable: {}", space_value);
 } 
