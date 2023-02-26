6aa460766a010a3cf5aeeb563585b9086b8f00b1
CemGurhan <cemg@live.co.uk>
2023-02-26 13:24:11 +0000

    added shadowing exaple

--- variables_and_shadowing/src/main.rs
+++ variables_and_shadowing/src/main.rs
@@ -3,5 +3,7 @@ fn main() {
     let x = 23;
     {
         let x = 23 * 2; 
+        println!("The value of x in inner scope is: {}", x);
     }
+    println!("The value of x in outer scope is: {}", x);
 } 
