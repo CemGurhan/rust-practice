d43595854dfd8ea0b95e9add9cc28a13bc4d8a16
CemGurhan <cemg@live.co.uk>
2023-02-28 16:34:33 +0000

    added else if example

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     if_statment_one(8);
     if_statment_one(1);
+    else_if_statement(1);
+    else_if_statement(4);
+    else_if_statement(111);
 }
 
 fn if_statment_one(x: u32) {
@@ -11,5 +14,20 @@ fn if_statment_one(x: u32) {
         println!("condition was true with argument: {}", x);
     } else {
         println!{"condition was false with argument: {}", x};
+    }
+}
+
+fn else_if_statement(x: u32) {
+    // else if statements can be useful for evaluating multiple conditions.
+    // They wont evaluate any further conditions past the fast true codition however
+    // and can get quite messy. Match statements can be useful as a cleaner alternative in that case
+    if x > 7 {
+        println!("else if first condition true with argument: {}", x);
+    } else if x >= 3 {
+        println!("else if second condition true with argument: {}", x);
+    } else if x > 5 {
+        println!("else if first condition true with argument: {}", x);
+    } else {
+        println!("no conditions true for argument: {}", x);
     }
 }
