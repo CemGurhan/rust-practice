665e85741209e2ff94b8bd3308e8e0bc77c08ea7
CemGurhan <cemg@live.co.uk>
2023-02-28 16:25:11 +0000

    added if statement example

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    if_statment_one(8);
+    if_statment_one(1);
+}
+
+fn if_statment_one(x: u32) {
+    // if statements allow for logic to be implemented depending on a condition.
+    // Conditions should always be evaluated with booleans, Rust does not automatically turn numbers into booleans.
+    // Logic that is ran after a condition is evaluated is doen in an if statement 'arm'.
+    if x > 7 {
+        println!("condition was true with argument: {}", x);
+    } else {
+        println!{"condition was false with argument: {}", x};
+    }
 }
