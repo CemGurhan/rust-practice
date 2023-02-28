ccd6cc2b8b344fda8842e853c4922982c7483a82
CemGurhan <cemg@live.co.uk>
2023-02-28 15:21:55 +0000

    added fucntion with aprameter

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,15 @@
 fn main() {
     println!("Hello from main");
     new_function_one();
+    new_function_two(5);
 }
 
+// functions in Rust must use snakecase with all lowercase characters
 fn new_function_one() {
     println!("Hello from new_function_one");
+}
+
+// functions can take parameters in the form of arguments upon the function call
+fn new_function_two(x: i32) {
+    println!("Value of argument provided as parameter: {}", x);
 }
