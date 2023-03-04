687b4aa75aa5dcc6eff5f51e41591a3a5142da51
CemGurhan <cemg@live.co.uk>
2023-03-04 08:07:42 +0000

    added understanding_ownership project and an example on variable scope

---
+++ understanding_ownership/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    
+    { // s is not valid here or before here
+        let s = "am I valid?" // s is valid here
+        // s is valid here
+        // s is valid here
+    }   // s is not valid from this point on 
+}
