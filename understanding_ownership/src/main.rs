806e7579abd22a6633da93f541692bf47ca9d991
CemGurhan <cemg@live.co.uk>
2023-03-04 08:30:03 +0000

    added comments and a string type function

--- understanding_ownership/src/main.rs
+++ understanding_ownership/src/main.rs
@@ -1,10 +1,23 @@
 fn main() {
+    // Three rules of Ownership in Rust:
+    // 1) every value must have an owner
+    // 2) there can be only one owner at a time
+    // 3) when an owner goes out of scope, it's value is dropped
     scope();
 }
 
 fn scope() {
+    // a scope is the range in which a value is valid in a Rust program
        { // s is not valid here or before here
         let s = "am I valid?"; // s is valid here onwards as it comes into scope
         println!("{} Yes!", s);        
     }   // s is not valid from this point on 
+}
+
+fn string_type() {
+    // String literals are data types that can be stored on the stack.
+    // Their size is known at compile time and is fixed as they are immutable.
+    // The String type is instead allocated to the heap. It's size is
+    // unknown at compile type and is mutable. 
+    // It is a complex data type.
 }
