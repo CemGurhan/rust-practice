7aa3bcfb9d73e97d403f4b09880a0a7b4213b36c
CemGurhan <cemg@live.co.uk>
2023-03-06 07:53:40 +0000

    added string slice return function example

--- the_slice_type/src/main.rs
+++ the_slice_type/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     string_slices();
+    let string1 = String::from("hello");
+    let string1_slice = returning_string_slice(&string1); // Rust now knows this reference's lifetime begins here as it is passed into the function
+    println!("&str returned from function: {}",string1_slice);
 }
 
 fn string_slices() {
@@ -13,4 +16,15 @@ fn string_slices() {
     // creating our slices. The slice borrows from a part of the 
     // string "hello world"
     println!("both slices together: {} {}", hello, world);
+}
+// The type of a String slice is &str, as can be seen from 
+// `returning_string_slice`'s return
+// With this function, the argument must be a reference to a String.
+// This is because we create a reference to a part of the String in
+// the function itself. Hence, when the function returns, Rust won't 
+// know where the lifetime of the reference to this part of the String 
+// began (where the reference came into scope).
+fn returning_string_slice(s: &String) -> &str {
+    let slice = &s[..];
+    slice
 }
