50e4f9cfcde79fe0f47f2eb286618a07497a2c57
CemGurhan <cemg@live.co.uk>
2023-03-06 08:17:50 +0000

    example of reference rule 1 error

--- the_slice_type/src/main.rs
+++ the_slice_type/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     string_slices();
     let string1 = String::from("hello");
     let string1_slice = returning_string_slice(&string1); // Rust now knows this reference's lifetime begins here as it is passed into the function
-    println!("&str returned from function: {}",string1_slice);
+    println!("&str returned from function: {}",string1_slice); 
 }
 
 fn string_slices() {
@@ -27,4 +27,25 @@ fn string_slices() {
 fn returning_string_slice(s: &String) -> &str {
     let slice = &s[..];
     slice
+}
+
+// If we uncomment out the `println!("the first word is: {}", word);` here,
+// we get a compilation error. 
+//
+// This is because the function `returning_string_slice` creates an
+// immutable reference to the String `s` and stores it in the variable
+// `word`. 
+//
+// If we now try to use the print statement after, the print statement is 
+// going to need the immutable reference `word`. However, if the variable
+// `s` that this immutable reference `word` refers to is cleared before this,
+// `word` has nothing to reference or borrow. Hence, we get a compilation error
+fn immutable_mutable_error() {
+    let mut s = String::from("hello world");
+
+    let word = returning_string_slice(&s);
+
+    s.clear(); // error!
+
+    // println!("the first word is: {}", word); 
 }
