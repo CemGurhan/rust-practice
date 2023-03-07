5a1d3c7d98c14d87f52fe84ba529c143a1b9f098
CemGurhan <cemg@live.co.uk>
2023-03-07 20:11:51 +0000

    added example of using a rectangle struct

--- example_program_using_structs/src/main.rs
+++ example_program_using_structs/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let rectangle1 = Rectangle { width: 10, height: 30 };
+
+    println!("Rectangle area: {}", calculate_area(&rectangle1));
+}
+
+struct Rectangle {
+    width: u64,
+    height: u64,
+}
+
+// Using a rectangle struct there helps group together
+// our length and width values together descriptively.
+// This can be very helpful when attempting to better understand the code,
+// as we can accurately convey the relationship between the width and height values
+
+// Taking an immutable reference to the rectangle allows us to 
+// call upon it's fields without moving it's values
+// Taking an immutable reference also allows us to continue using
+// the Rectangle struct in main.
+fn calculate_area(rectangle: &Rectangle) -> u64 {
+    rectangle.height * rectangle.width
 }
