11bbbea0fcbcb0431ed787185e72d0ef9959431b
CemGurhan <cemg@live.co.uk>
2023-03-07 07:16:09 +0000

    added example of struct instance immutability and mutability

--- defining_and_instantiating_structs/src/main.rs
+++ defining_and_instantiating_structs/src/main.rs
@@ -1,25 +1,48 @@
 fn main() {
     defining_structs();    
+    struct_mutability();
 }
 
 fn defining_structs() {
     // A struct is a custom data type that allows you to group
-    // together different types of data in the form of related values 
+    // together different related values. These values can also
+    // be of different data types.
     
 
     // structs are similar to tuples. They can both store values with
     // different data types together. However, they are unlike each other 
     // in the sense that you must name each piece of data inside of a struct
-
     struct User {
         name : String,
         email : String,
         sign_in_count : u64,
     }
 
-    let user1 = User { 
+    let _user1 = User { 
         name: String::from("username"), 
         email: String::from("an_email"), 
         sign_in_count: 1 };
+}
 
+fn struct_mutability() {
+    // struct instances are immutable by default. In order to make an instance
+    // mutable we must use the `mut` keyword upon declaration. 
+    // You cannot make individual fields mutable in a struct instance, you must
+    // make the entire instance mutable instead. 
+    struct User {
+        name : String,
+        email : String,
+        sign_in_count : u64,
+    }
+
+    let mut mutable_user = User { 
+        name: String::from("Change me!"), 
+        email: String::from("Change me!"), 
+        sign_in_count: 1 };
+    
+    println!("Old email: {}", mutable_user.email);
+
+    mutable_user.email = String::from("new_email@email.com");
+
+    println!("New email: {}", mutable_user.email);
 }
