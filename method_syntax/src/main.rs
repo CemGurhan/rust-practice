9286371f675176be6aa0e7b9cfa950bc35df3051
CemGurhan <cemg@live.co.uk>
2023-03-08 07:55:42 +0000

    gave example of impl block and method syntax

--- method_syntax/src/main.rs
+++ method_syntax/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    let rectangle1 = Rectangle {
+        width: 50,
+        height: 70
+    };
+    println!("area of rectangle: {}", rectangle1.area());
 }
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u64,
+    height: u64
+}
+
+// We can use impl blocks to organize everything that
+// relates to a particular struct
+// `self` is an alias for whatever struct the impl block
+// was created for. It is shorthand for self: Self 
+// (in our case self: &Self, which is analagous to
+// rectangle: &Rectangle)
+// we can take ownership of self, immutably borrow 
+// or mutably morrow from self
+impl Rectangle {
+    fn area(&self) -> u64 {
+        self.height * self.width
+    }
+}
+
+
