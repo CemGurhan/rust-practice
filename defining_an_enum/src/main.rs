fdc1828a8446c0b6649499da45a1e12a46cc6452
CemGurhan <cemg@live.co.uk>
2023-03-08 19:29:10 +0000

    added example of instantiating an enum

---
+++ defining_an_enum/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    defining_enums();
+}
+
+fn defining_enums() {
+    // Enums allow you to define a type by 
+    // enumerating it's possible variants.
+    // An enum can be one type that has a
+    // set of possible values
+    enum IpAddrKind {
+        V4,
+        V6
+    }
+}
