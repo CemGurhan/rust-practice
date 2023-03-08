88c984537ab73c62076cbbb4198d9a84b6b51def
CemGurhan <cemg@live.co.uk>
2023-03-08 06:46:43 +0000

    added example of the Debug trait

--- example_program_using_structs/src/main.rs
+++ example_program_using_structs/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let rectangle2 = (200, 10);
 
     println!("Rectangle area with: {}", calculate_area_tuple(rectangle2));
+    debug_display();
 }
 
 struct Rectangle {
@@ -31,4 +32,36 @@ fn calculate_area(rectangle: &Rectangle) -> u64 {
 // to do our best to understand what they represent:
 fn calculate_area_tuple(tuple: (u32, u32)) -> u32 {
     tuple.0 * tuple.1
+}
+
+fn debug_display() {
+    // You cannot print out a struct in the following way:
+    // println!("{}", struct);
+    // This is because structs do not implement `Display`.
+    // `Display` is the default output type for the `println!`
+    // macro once it uses the `{}` placeholder.
+    // There are multiple ways one could format the display
+    // of a struct and Rust would not be able to guess what
+    // the user wants.
+
+    // Instead, you can use the `Debug` trait, which can be 
+    // used with the `println!` macro with the placeholder
+    // `{:?}`. In order to use a struct with this trait,
+    // you need to add the attribute `#[derive(Debug)]` once
+    // it's instantiated.
+
+    #[derive(Debug)]
+    struct Hobbit {
+        name : String,
+        surname: String,
+        age: u32
+    }
+
+    let person1 = Hobbit {
+        name : String::from("Bilbo"),
+        surname : String::from("Baggins"),
+        age : 111
+    };
+
+    println!("Printing struct with the `Debug` trait: {:?}", person1);
 }
