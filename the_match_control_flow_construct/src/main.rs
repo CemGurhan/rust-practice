d6ad209b128d1828743865ce103f94fb2449829e
CemGurhan <cemg@live.co.uk>
2023-03-09 20:27:14 +0000

    gave examples of catch-all patterns

--- the_match_control_flow_construct/src/main.rs
+++ the_match_control_flow_construct/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let absent_number : Option<i32> = None;
     let _six = matching_options(five);
     let _absent = matching_options(absent_number);
+    catch_all_pattern();
 }
 
 enum Coin {
@@ -92,3 +93,56 @@ fn matching_options(x: Option<i32>) -> Option<i32> {
     // enters our match expression, such that we can execute logic on 
     // i32 value that the entering Some(i32) variant contained.
 }
+
+fn catch_all_pattern() {
+    // If we want a pattern that can be useful for implementing
+    // some logic for all potential values of a type, we can use
+    // the catch-all pattern.
+    //
+    // Let's say we have a game, where rolling a 3 on a die results
+    // in the player putting on a hat, and rolling a 7 taking the
+    // hat off. If you roll anything else, you just move your
+    // piece by the amount on the dice.
+    let dice_roll = 9;
+    match dice_roll {
+        3 => put_on_hat(),
+        7 => take_off_hat(),
+        other => move_piece(other),
+    };
+    // Here, the `other` variable can represent any i32 value.
+    // The pattern binds to the value passed in as `dice_roll`,
+    // which means that `other` takes whatever value dice_roll is
+    // and is passed to the move_piece() function.
+    // This match expression is exhaustive as other accounts for 
+    // all other possible i32 values.
+    //
+    // If we didn't want the pattern to bind to any value, or 
+    // have that value be left unused if we didn't use it in
+    // the arm's code, we could use the `_` pattern.
+    // Let's say our player has to re roll if they dont get 3 or
+    // 7.
+    match dice_roll {
+        3 => put_on_hat(),
+        7 => take_off_hat(),
+        _ => re_roll(),
+    };
+    // This match expression is still exhaustive, as we are still
+    // covering all other possible values of `dice_roll`. We are
+    // basically telling the expression to ignore all other i32
+    // values passing through and upon ignoring, execute `re_roll()`
+    //
+    // We can also have the code do nothing after this catch-all
+    // pattern. For this, we can use the unit value `()`.
+    // We can have the expression ignore all other values coming
+    // in, and do nothing after ignoring them.
+    match dice_roll {
+        3 => put_on_hat(),
+        7 => take_off_hat(),
+        _ => (),
+    }
+}
+
+fn put_on_hat() {}
+fn take_off_hat() {}
+fn move_piece(amount: i32) {}
+fn re_roll() {}
