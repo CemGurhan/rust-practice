d6efba59dbd58b0b420b26f422fa39cecce1434b
CemGurhan <cemg@live.co.uk>
2023-03-09 20:54:11 +0000

    added example of `if let` use instead of match expression

--- concise_flow_with_if_let/src/main.rs
+++ concise_flow_with_if_let/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    if_let();
+}
+
+fn if_let() {
+    // When a match expression is too verbose, an `if let` 
+    // can be useful as an alternative. `if let` is syntax 
+    // sugar for a match expression that runs code only on
+    // on particular pattern and ignores everything else.
+    let config_max = Some(33);
+    match config_max {
+        Some(max) => println!("Max value is: {}", max),
+        _ => ()
+    };
+    // Can be represented by an `if let` as such:
+    if let Some(max) = config_max {
+        println!("Max value is: {}", max);
+    }
 }
