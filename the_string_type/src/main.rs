0aa8635c087f4e818193074e1cfb2ca7921d8054
CemGurhan <cemg@live.co.uk>
2023-03-14 13:12:35 +0000

    add example of String creation and similarity to vectors

--- the_string_type/src/main.rs
+++ the_string_type/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    create_string();
+}
+
+// The String type has many similar properties to a vector,
+// as it acts as a wrapper around a Vector of bytes. 
+fn create_string() {
+    let mut s1 = String::new();
+    s1 = "hi".to_string();
+    // we can condense this down with the String::from() method
+    {
+        let _s1 = String::from("hi");
+    }
 }
