8c54d4f13f2cade732d081e192816487e394b06f
CemGurhan <cemg@live.co.uk>
2023-03-14 13:27:16 +0000

    show example of updating strings with `push` and `push_str`

--- the_string_type/src/main.rs
+++ the_string_type/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     create_string();
+    updating_a_string();
 }
 
 // The String type has many similar properties to a vector,
@@ -11,4 +12,18 @@ fn create_string() {
     {
         let _s1 = String::from("hi");
     }
+}
+
+fn updating_a_string() {
+    // We can update a string using `push_str`
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("Mutated string with `push_str`: {}", s);
+    
+    // We can update a string with a single character using
+    // `push`
+    let mut s2 = String::from("by");
+    s2.push('e');
+    println!("Mutated string with `push`: {}", s2);
+
 }
