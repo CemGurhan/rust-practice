f9c622768af28ea8bb88554d4411d3accc8a7f20
CemGurhan <cemg@live.co.uk>
2023-03-15 07:24:39 +0000

    add example of iterating over HashMaps

--- hashmaps/src/main.rs
+++ hashmaps/src/main.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 fn main() {
     new_hashmap();
     accessing_values_in_hashmaps();
+    iterating_over_hashmaps();
 }
 
 fn new_hashmap() {
@@ -27,4 +28,15 @@ fn accessing_values_in_hashmaps() {
     // this `None` to 0 if the value for that key doesnt exist.
    let your_socre = scores.get(&your_team_name).copied().unwrap_or(0);
    println!("Your teams score: {}", your_socre);
+}
+
+fn iterating_over_hashmaps() {
+    // we can iterate of HashMap values using for loops
+    let mut scores = HashMap::new();
+    scores.insert("wow", 20);
+    scores.insert("woah", 25);
+
+    for (key, value) in scores {
+        println!("{key}: {value}");
+    }
 }
