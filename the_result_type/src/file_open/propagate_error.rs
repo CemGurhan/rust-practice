17040580be62dc797d9757ba4e75d962298001e6
CemGurhan <cemg@live.co.uk>
2023-03-19 16:50:20 +0000

    add further example with `?` operator

--- the_result_type/src/file_open/propagate_error.rs
+++ the_result_type/src/file_open/propagate_error.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Read};
+use std::{fs, fs::File, io::Read};
 
 // We can use error propagation to return errors back
 // to the caller of a function. This way, the error can
@@ -20,3 +20,31 @@ pub fn read_username_from_file() -> Result<String, std::io::Error> {
         Err(e) => Err(e)
     }
 }
+
+// We can shorten this code using the `?` operator. The `?` 
+// operator will either return a value from the operation to
+// a variable if an `Ok` is found, or will return from the 
+// entire function with an error if an `Err` is found
+pub fn read_username_from_file_shortened_one() -> Result<String, std::io::Error> {
+    let mut username_file = File::open("username.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+// We can shorten this even further using chaining
+pub fn read_username_from_file_shortened_two() -> Result<String, std::io::Error> {
+    let mut username = String::new();
+    File::open("username.txt")?.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+// We can shorten this even further using fs::read_to_string,
+// which opens a file, creates a new string and reads the file
+// to the new string
+pub fn read_username_from_file_fs_function() -> Result<String, std::io::Error> {
+    fs::read_to_string("username.txt")
+}
+
+
+
