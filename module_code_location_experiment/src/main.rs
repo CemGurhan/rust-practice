0390a29f2190e3bbd881c691e4672d088e85c681
CemGurhan <cemg@live.co.uk>
2023-03-19 12:13:18 +0000

    add example of module storage

--- module_code_location_experiment/src/main.rs
+++ module_code_location_experiment/src/main.rs
@@ -1,3 +1,8 @@
+// A module defined in the crate root will have
+// to have it's code stored in the `src` directory
+// You cant do this:
+// mod something_to_store;
+
 fn main() {
     println!("Hello, world!");
 }
