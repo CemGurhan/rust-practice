c6e5d9e6b4007f6c073301e8e30191701546031c
Cem Gurhan <cemg@live.co.uk>
2023-03-20 15:04:16 +0000

    add example of generic types on impl vs on methods

--- generic_data_types/src/generic_type_methods.rs
+++ generic_data_types/src/generic_type_methods.rs
@@ -26,3 +26,20 @@ impl Point<f32> {
 }
 // The method `distance_from_origin` can only be used on
 // types with `f32` in place of their generic type
+
+// You can have a set of generic type parameters be relevent 
+// to an item and have another set be relevent to a method on
+// that item
+struct Wow<X1,Y1>{
+    woah: X1,
+    wowz: Y1,
+}
+
+impl<X1,Y1> Wow<X1,Y1>{
+    fn changes<X2,Y2>(self,other: Wow<X2, Y2>) -> Wow<X1, Y2> {
+        Wow {
+            woah: self.woah, 
+            wowz: other.wowz
+        }
+    }
+}
