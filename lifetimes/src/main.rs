2e07cf93f977f7f7c82f7396ecfe70668c0b1ceb
Cem Gurhan <cemg@live.co.uk>
2023-03-22 15:20:30 +0000

    add example of lifetime annotation syntax and generic lifetime parameters

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,3 +1,30 @@
+// As we can see, the lifetime of `result` is less than the overlap of x and y's lifetimes ('a)
+
 fn main() {
-    println!("Hello, world!");
+    let string1 = String::from("hi");      // ------------------------- x 
+    {                                                                       //  |  
+        let string2 = String::from("bye"); // ------------'a  y     //  |  
+        let result = longest(&string1, &string2);  //   |  |     //  |  
+        println!("Longest string: {}", result);               //   |  |     //  |   
+    } // -------------------------------------------------------------      //  |  
+}  // --------------------------------------------------------------------------- 
+
+// We can use the lifetime annotation syntax to define
+// the relationship between the lifetimes of multiple 
+// references. 
+// We can use the lifetime annotation syntax in a function
+// with a generic lifetime parameter
+//
+// In this function, we are saying that the parameters to and
+// return from this function must all be valid for as long as 
+// at least the lifetime `'a`. 
+// The lifetime `'a` is as long as the overlap between the lifetimes
+// of x and y. The return value must have a lifetime smaller than this
+// overlap
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
